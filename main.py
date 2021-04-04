77b0bb3e82533a89cd8692f0eb56de78cd234528
idhaya55 <72275684+idhaya55@users.noreply.github.com>
2021-04-05 02:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', 'sqlite:///cafes.db')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///cafes.db")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
